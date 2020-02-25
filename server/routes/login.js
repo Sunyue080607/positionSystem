49987d7086b91deae9f4b8ff29d4739b7255778f
孙跃 <kallensun@sunyuedeMacBook-Pro.local>
2020-02-26 03:27:28 +0800

    连接数据库完成

--- server/routes/login.js
+++ server/routes/login.js
@@ -1,11 +1,17 @@
 const express=require('express');
 const router=express.Router();
-const db=require('../config/db')
+const db=require('../config/db');
 
-
-router.use('/',(req,res)=>{
+router.use('/',(req,res)=> {
     console.log(req.body)
-    res.send('helloworld')
+    let sql = "select * from User"
+    db.query(sql, (err, result) => {
+        for(let item of result){
+            if(item.username===req.body.username&&item.password===req.body.password){
+                res.send(200)
+            }
+        }
+    })
 })
 
 
